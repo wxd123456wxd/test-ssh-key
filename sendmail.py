c8afd4fbcdd5c5b74b61d791b58af1a6f4e13330
wxd <wxd@ahbcht.com>
2018-12-27 15:12:21 +0800

    modify sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -47,6 +47,16 @@ def new_report(test_dir):
     # 获取最新文件的绝对路径
     file_path = os.path.join(test_dir, lists[-1])
     return file_path  # 发送邮件
+	
+	#def new_report(test_dir):
+     # 列举test_dir目录下的所有文件，结果以列表形式返回。
+    #lists = os.listdir(test_dir)
+    # sort按key的关键字进行排序，lambda的入参fn为lists列表的元素，获取文件的最后修改时间
+    # 最后对lists元素，按文件修改时间大小从小到大排序。
+    #lists.sort(key=lambda fn: os.path.getmtime(test_dir + '\\' + fn))
+    # 获取最新文件的绝对路径
+    #file_path = os.path.join(test_dir, lists[-1])
+    #return file_path  # 发送邮件
 
 
 
